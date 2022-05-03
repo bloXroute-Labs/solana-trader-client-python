6882c7157faecdbd5f34d4bc49a251ae419f3124
Kaushik Donthi <kaushikdr@gmail.com>
2022-05-02 19:42:40 -0700

    Created streaming test with timeout for 3 calls and reusable helper function

--- bxserum/test/integration/stream.py
+++ bxserum/test/integration/stream.py
@@ -1,16 +1,21 @@
 import asyncio
-import bxserum
 from async_timeout import timeout
+import bxserum
 from unittest import TestCase
+from typing import AsyncGenerator
+
+stream_expect_entries = 3
+stream_expect_timeout = 60
+
+async def test_stream(t: TestCase, stream: AsyncGenerator):
+    try:
+        for i in range(stream_expect_entries):
+            async with timeout(stream_expect_timeout):
+                await stream.__anext__()
+    except asyncio.TimeoutError:
+        t.fail("no value found in " + str(stream_expect_timeout) + " seconds")
+
 
 async def test_orderbook_stream(t: TestCase, p: bxserum.Api):
-    market_name = "SOL/USDC"
-    try:
-        async with timeout(30):
-            async for orderbook in p.get_orderbook_stream(market=market_name):
-                t.assertIsNotNone(orderbook)
-                t.assertEqual(orderbook.orderbook.market, market_name)
-                return
-    except asyncio.TimeoutError:
-        t.fail("no value found in 30 seconds")
-
+    stream = p.get_orderbook_stream(market="SOL/USDT")
+    await test_stream(t, stream)
