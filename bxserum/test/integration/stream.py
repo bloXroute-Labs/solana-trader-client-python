d643367d7675f6777bd7e376fd7ce7ba362d63a3
Kaushik Donthi <kaushikdr@gmail.com>
2022-05-02 20:05:24 -0700

    Reduce stream expected entries from 3 to 2

--- bxserum/test/integration/stream.py
+++ bxserum/test/integration/stream.py
@@ -4,7 +4,7 @@ import bxserum
 from unittest import TestCase
 from typing import AsyncGenerator
 
-stream_expect_entries = 3
+stream_expect_entries = 2
 stream_expect_timeout = 60
 
 async def test_stream(t: TestCase, stream: AsyncGenerator):
@@ -17,5 +17,5 @@ async def test_stream(t: TestCase, stream: AsyncGenerator):
 
 
 async def test_orderbook_stream(t: TestCase, p: bxserum.Api):
-    stream = p.get_orderbook_stream(market="SOL/USDT")
+    stream = p.get_orderbook_stream(market="SOL/USDC")
     await test_stream(t, stream)
