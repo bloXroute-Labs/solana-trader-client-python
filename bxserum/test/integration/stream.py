9eed24129f2486096bf23a9aef063477363fdbdd
Kaushik Donthi <kaushikdr@gmail.com>
2022-05-02 18:49:08 -0700

    Fixed orderbook stream test

--- bxserum/test/integration/stream.py
+++ bxserum/test/integration/stream.py
@@ -1,13 +1,16 @@
+import asyncio
+import bxserum
+from async_timeout import timeout
 from unittest import TestCase
-from bxserum import provider
 
-async def test_orderbook_stream(t: TestCase, p: provider.Provider):
-    counter = 0
-    market_name = "SOLUSDC"
-    async for orderbook in p.get_orderbook_stream(market=market_name):
-        t.assertIsNotNone(orderbook)
-        t.assertEqual(orderbook.orderbook.market, market_name)
+async def test_orderbook_stream(t: TestCase, p: bxserum.Api):
+    market_name = "SOL/USDC"
+    try:
+        async with timeout(30):
+            async for orderbook in p.get_orderbook_stream(market=market_name):
+                t.assertIsNotNone(orderbook)
+                t.assertEqual(orderbook.orderbook.market, market_name)
+                return
+    except asyncio.TimeoutError:
+        t.fail("no value found in 30 seconds")
 
-        counter += 1
-        if counter == 3:
-            return
