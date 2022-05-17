7292bb8039c880121413a47e860c7750887157ee
Kaushik Donthi <kaushikdr@gmail.com>
2022-05-17 12:40:46 -0700

    `key_pair` -> `keypair`

--- bxserum/transaction/signing.py
+++ bxserum/transaction/signing.py
@@ -14,43 +14,43 @@ def sign_tx(unsigned_tx_base64: str) -> str:
     # convert base58 private key string to a keypair
     pkey_bytes = bytes(pkey_str, encoding="utf-8")
     pkey_bytes_base58 = base58.b58decode(pkey_bytes)
-    key_pair = Keypair.from_secret_key(pkey_bytes_base58)
+    keypair = Keypair.from_secret_key(pkey_bytes_base58)
 
-    return sign_tx_with_private_key(unsigned_tx_base64, key_pair)
+    return sign_tx_with_private_key(unsigned_tx_base64, keypair)
 
 # sign_tx_with_private_key uses the provided private key to sign the message content and replace the zero signature
-def sign_tx_with_private_key(unsigned_tx_base64: str, key_pair: Keypair) -> str:
+def sign_tx_with_private_key(unsigned_tx_base64: str, keypair: Keypair) -> str:
     # convert base64 transaction string to a transaction
     tx_bytes = bytes(unsigned_tx_base64, encoding="utf-8")
     tx_bytes_base64 = base64.decodebytes(tx_bytes)
     tx = Transaction.deserialize(tx_bytes_base64)
 
     # sign transaction using keypair
-    _sign_tx(tx, key_pair)
+    _sign_tx(tx, keypair)
 
     # convert transaction back to base64
     signed_tx_bytes_base64 = base64.b64encode(tx.serialize())
     return signed_tx_bytes_base64.decode("utf-8")
 
-def _sign_tx(tx: Transaction, key_pair: Keypair):
+def _sign_tx(tx: Transaction, keypair: Keypair):
     signatures_required = tx.compile_message().header.num_required_signatures
     signatures_present = len(tx.signatures)
     if signatures_present != signatures_required:
         raise Exception(f"transaction requires {signatures_required} signatures and has {signatures_present} signatures")
 
-    _replace_zero_signature(tx, key_pair)
+    _replace_zero_signature(tx, keypair)
 
-def _replace_zero_signature(tx: Transaction, key_pair: Keypair):
+def _replace_zero_signature(tx: Transaction, keypair: Keypair):
     message_content = tx.serialize_message()
-    signed_message_content = key_pair.sign(message_content)
+    signed_message_content = keypair.sign(message_content)
     new_signature = signed_message_content.signature
 
     if not tx.signatures:
         raise Exception("transaction does not have any signatures")
 
     zero_sig_index = -1
-    for index, pub_key_pair in enumerate(tx.signatures):
-        if pub_key_pair.signature == None:
+    for index, pub_keypair in enumerate(tx.signatures):
+        if pub_keypair.signature == None:
             if zero_sig_index != -1:
                 raise Exception("more than one zero signature provided in transaction")
             zero_sig_index = index
