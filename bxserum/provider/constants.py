028cdb1588ad84549bbd60200f16b5ab973bc8c1
Kevin Chen <kevin@bloxroute.com>
2022-06-17 13:47:27 -0500

    BX-4384 update dns entries (#10)

--- bxserum/provider/constants.py
+++ bxserum/provider/constants.py
@@ -3,9 +3,9 @@ MAINNET_API_WS = "wss://virginia.solana.dex.blxrbdn.com/ws"
 MAINNET_API_GRPC_HOST = "virginia.solana.dex.blxrbdn.com"
 MAINNET_API_GRPC_PORT = 9000
 
-TESTNET_API_HTTP = "http://44.200.244.204:80"
-TESTNET_API_WS = "ws://44.200.244.204:80/ws"
-TESTNET_API_GRPC_HOST = "44.200.244.204"
+TESTNET_API_HTTP = "http://serum-nlb-53baf45ef9775263.elb.us-east-1.amazonaws.com/"
+TESTNET_API_WS = "ws://serum-nlb-53baf45ef9775263.elb.us-east-1.amazonaws.com/ws"
+TESTNET_API_GRPC_HOST = "serum-nlb-53baf45ef9775263.elb.us-east-1.amazonaws.com:9000"
 TESTNET_API_GRPC_PORT = 443
 
 LOCAL_API_HTTP = "127.0.0.1:9000"
