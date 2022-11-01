6db927f77152eb18e572b58b39f2b4d354cdd9b6
Mahmoud Taabodi <mahmoud.taabodi@bloxroute.com>
2022-11-01 15:32:01 -0400

    first part

--- bxsolana/transaction/memo.py
+++ bxsolana/transaction/memo.py
@@ -2,6 +2,7 @@ import base64
 
 from solana import transaction as solana_transaction, keypair
 from solana.publickey import PublicKey
+from solana.keypair import Keypair
 from solana.blockhash import Blockhash
 from solana.message import CompiledInstruction, Message, MessageArgs, MessageHeader
 
@@ -12,42 +13,48 @@ class Memo:
 
     # create_trader_api_memo_instruction generates a transaction instruction that places a memo in the transaction log
     # Having a memo instruction with signals Trader-API usage is required
-    def create_trader_api_memo_instruction(self, msg):
+    @staticmethod
+    def create_trader_api_memo_instruction(msg):
         if msg == "":
-            msg = self.BxMemoMarkerMsg
+            msg = Memo.BxMemoMarkerMsg
 
         data = bytes(msg, 'utf-8')
-        instruction = solana_transaction.TransactionInstruction(keys=[], program_id=self.TraderAPIMemoProgram,
+        instruction = solana_transaction.TransactionInstruction(keys=[], program_id=Memo.TraderAPIMemoProgram,
                                                                 data=data)
+
         return instruction
 
-    def add_memo(self, instructions, memo_content, blockhash, owner, private_keys):
-        memo = self.create_trader_api_memo_instruction(memo_content)
+    @staticmethod
+    def add_memo(instructions, memo_content: str, blockhash, owner, private_keys):
+        memo = Memo.create_trader_api_memo_instruction(memo_content)
 
         instructions.append(memo)
 
-        txn_bytes, err = self.build_fully_signed_txn(blockhash, owner, instructions, private_keys)
+        txn_bytes, err = Memo.build_fully_signed_txn(blockhash, owner, instructions, private_keys)
         if err is not None:
-            return "", err
+            return ""
 
-        return base64.encodebytes(txn_bytes), None
+        return base64.encodebytes(txn_bytes)
 
     # add_memo_to_serialized_txn adds memo instruction to a serialized transaction, it's primarily used if the user
     # doesn't want to interact with Trader-API directly
-    def add_memo_to_serialized_txn(self, tx_base64, memo_content, owner, private_keys):
+    @staticmethod
+    def add_memo_to_serialized_txn(tx_base64: str, memo_content: str, owner, private_keys: Keypair):
         tx_bytes = bytes(tx_base64, encoding="utf-8")
         tx_bytes_base64 = base64.decodebytes(tx_bytes)
 
         tx = solana_transaction.Transaction.deserialize(tx_bytes_base64)
 
-        return self.add_memo(tx.instructions, memo_content, tx.recent_blockhash, owner, private_keys)
+        return Memo.add_memo(tx.instructions, memo_content, tx.recent_blockhash, owner, private_keys)
 
-    def build_fully_signed_txn(self, recent_block_hash, owner, instructions, private_keys):
+    @staticmethod
+    def build_fully_signed_txn(recent_block_hash, owner, instruction: solana_transaction.TransactionInstruction, private_keys: Keypair):
 
         tx = solana_transaction.Transaction(recent_blockhash=recent_block_hash)
 
-        tx.instructions.append(instructions)
+        tx.instructions.append(instruction)
+
         tx.fee_payer = owner
         tx.sign(private_keys)
 
-        return tx.serialize(), None
+        return tx.serialize()
---
+++ bxsolana/transaction/memo_test.py
@@ -0,0 +1,32 @@
+import base64
+import unittest
+
+import base58
+from solana.keypair import Keypair
+from .memo import Memo
+
+from solana.publickey import PublicKey
+from solana.blockhash import Blockhash
+
+# key generated for this test
+RANDOM_PRIVATE_KEY = "3KWC65p6AvMjvpR2r1qLTC4HVSH4jEFr5TMQxagMLo1o3j4yVYzKsfbB3jKtu3yGEHjx2Cc3L5t8wSo91vpjT63t"
+# UNSIGNED_TX_BASE64 = "AgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQD6Amzo3TOF19nBfTBPCjx2oV1dRi5AtFA3kxVbfyOai9/PDbvr0d9iYkVVYCo+5heOy6m60Ua60t+EF+kXEJAgAFD4ls26fgpAnCYufUzDrXMMpDjMYkf2Y2FHuxqKE+2+IrRVKOQVHKKvreZyvh3wca8QpEP1VhjdfPmQtxZk41vr2EwvsYrtYZ9UZjJlPvBgKfAqhkvzgphnGBuyDfHXFcMEoHX5xmEhJgK0YZx3BKh/s3nhpE7IFyBzqsKBqiTDd6jfzI9XsPznt1ZnWa9u9nVKg1KibD5ElrzSfbftYpluJAIIlGU8/d+nt+YMlmaCc2otsPg4VkklsRB3oh4DbXlwD0JuFuuM8DEZF1+YBRQ0SVXONw52WUDzwpQ5VF+0Wppt/RXFB3Bfkzm5U8Gk39vJzBht0vYt9IqVgEXip2UlkfJvXwRhxAEL1cyMpwZt2lhKbucXk0xnet9MJfvRVqLWrj7TJ6D4hJp3KUHZcFDzpujLjdOrzbFHCIfIK1TT82BpuIV/6rgYT7aH9jRhjANdrEOdwa6ztVmKDwAAAAAAEGp9UXGSxcUSGMyUw9SvF/WNruCJuh/UTj29mKAAAAAAbd9uHXZaGT2cvhRs7reawctIXtX1s3kTqM9YV+/wCpAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACFDy1uAqR6+CTQmradxC1wyyjL+iSft+5XudJWwSdi7817DQWO+TIR4kugIb5dD6FCxudqoRDfwOC8xhRQk5dqBA0CAAE0AAAAAIB3jgYAAAAApQAAAAAAAAAG3fbh12Whk9nL4UbO63msHLSF7V9bN5E6jPWFfv8AqQwEAQoACwEBDgwCAwQFBgcBAAgJDAszAAoAAAABAAAAwAslCgAAAAABAAAAAAAAAACXePYDAAAAAAAAAAAAAAAAAAAAAAAAAP//DAMBAAABCQ=="
+# EXPECTED = b"AjRa6pZdYfbPV1vu4H+dziK+qYoeS4rPJX1B8rHYRo3p7a57JsRkgKe14FC+YWwEgq2i9onrSkTV8FTDph7rhg0QD6Amzo3TOF19nBfTBPCjx2oV1dRi5AtFA3kxVbfyOai9/PDbvr0d9iYkVVYCo+5heOy6m60Ua60t+EF+kXEJAgAFD4ls26fgpAnCYufUzDrXMMpDjMYkf2Y2FHuxqKE+2+IrRVKOQVHKKvreZyvh3wca8QpEP1VhjdfPmQtxZk41vr2EwvsYrtYZ9UZjJlPvBgKfAqhkvzgphnGBuyDfHXFcMEoHX5xmEhJgK0YZx3BKh/s3nhpE7IFyBzqsKBqiTDd6jfzI9XsPznt1ZnWa9u9nVKg1KibD5ElrzSfbftYpluJAIIlGU8/d+nt+YMlmaCc2otsPg4VkklsRB3oh4DbXlwD0JuFuuM8DEZF1+YBRQ0SVXONw52WUDzwpQ5VF+0Wppt/RXFB3Bfkzm5U8Gk39vJzBht0vYt9IqVgEXip2UlkfJvXwRhxAEL1cyMpwZt2lhKbucXk0xnet9MJfvRVqLWrj7TJ6D4hJp3KUHZcFDzpujLjdOrzbFHCIfIK1TT82BpuIV/6rgYT7aH9jRhjANdrEOdwa6ztVmKDwAAAAAAEGp9UXGSxcUSGMyUw9SvF/WNruCJuh/UTj29mKAAAAAAbd9uHXZaGT2cvhRs7reawctIXtX1s3kTqM9YV+/wCpAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACFDy1uAqR6+CTQmradxC1wyyjL+iSft+5XudJWwSdi7817DQWO+TIR4kugIb5dD6FCxudqoRDfwOC8xhRQk5dqBA0CAAE0AAAAAIB3jgYAAAAApQAAAAAAAAAG3fbh12Whk9nL4UbO63msHLSF7V9bN5E6jPWFfv8AqQwEAQoACwEBDgwCAwQFBgcBAAgJDAszAAoAAAABAAAAwAslCgAAAAABAAAAAAAAAACXePYDAAAAAAAAAAAAAAAAAAAAAAAAAP//DAMBAAABCQ=="
+
+
+class TestMemo(unittest.TestCase):
+    def test_adding_memo_to_serialized_tx(self):
+        pkey_bytes = bytes(RANDOM_PRIVATE_KEY, encoding="utf-8")
+        pkey_bytes_base58 = base58.b58decode(pkey_bytes)
+        kp = Keypair.from_secret_key(pkey_bytes_base58)
+
+        instruction = Memo.create_trader_api_memo_instruction("hi from dev")
+
+        recent_block_hash = Blockhash(str(PublicKey(3)))
+        tx_serialized = Memo.build_fully_signed_txn(recent_block_hash, kp.public_key, instruction, kp)
+
+        txbase64_str = base64.encodebytes(tx_serialized).decode('utf-8')
+        print("txbase64_str", txbase64_str)
+
+        tx_bytes = Memo.add_memo_to_serialized_txn(txbase64_str, "hi from dev2", kp.public_key,  kp)
+        print("tx_bytes", tx_bytes)
