b82c382c9bbdf9d127291ce68578a97f26e81c60
atulsriv <114938606+atulsriv@users.noreply.github.com>
2022-12-19 13:28:12 -0500

    TRAD-385 Refactor Python SDK Integration Tests into modules  (#42)

---
+++ bxsolana/examples/constants.py
@@ -0,0 +1,7 @@
+# sample keys to run integration/regression tests with
+# maintained by bloxRoute team
+PUBLIC_KEY = "4JCZomAb4eKcJQ9fqoi2JPT1TzjbhJEk8V8MmFKUQfnV"
+USDC_WALLET = "8w1igsiZfsWnMTJSKkSvDQYYCZyf6aXMihuXEYpxcZVD"
+OPEN_ORDERS = "4JCZomAb4eKcJQ9fqoi2JPT1TzjbhJEk8V8MmFKUQfnV"
+SOL_USDC_MARKET = "9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT"
+ORDER_ID = "3929156487700134707538850"
---
+++ bxsolana/examples/stream_utils.py
@@ -0,0 +1,39 @@
+import bxsolana
+from bxsolana_trader_proto import api as proto
+
+
+async def do_stream(api: bxsolana.Provider, run_slow=False):
+    item_count = 0
+
+    if run_slow:
+        print("streaming orderbook updates...")
+        async for response in api.get_orderbooks_stream(
+            markets=["SOLUSDC"], project=proto.Project.P_OPENBOOK
+        ):
+            print(response.to_json())
+            item_count += 1
+            if item_count == 5:
+                item_count = 0
+                break
+
+    if run_slow:
+        print("streaming ticker updates...")
+        async for response in api.get_tickers_stream(
+            market="SOLUSDC", project=proto.Project.P_OPENBOOK
+        ):
+            print(response.to_json())
+            item_count += 1
+            if item_count == 5:
+                item_count = 0
+                break
+
+    if run_slow:
+        print("streaming trade updates...")
+        async for response in api.get_trades_stream(
+            market="SOLUSDC", project=proto.Project.P_OPENBOOK
+        ):
+            print(response.to_json())
+            item_count += 1
+            if item_count == 1:
+                item_count = 0
+                break
