5d5ae896fe2975e32e0d08fb2e50635ef1bb60eb
tinystarinagalaxy <110410938+tinystarinagalaxy@users.noreply.github.com>
2023-01-12 13:42:07 -0500

    Adding GetMarketDepth example (#48)

--- bxsolana/examples/request_utils.py
+++ bxsolana/examples/request_utils.py
@@ -11,6 +11,15 @@ async def do_requests(
     usdc_wallet: str,
     sol_usdc_market: str,
 ):
+    print("fetching market depth")
+    print(
+        (
+            await api.get_market_depth(
+                limit=1, market="SOLUSDC", project=proto.Project.P_OPENBOOK
+            )
+        ).to_json()
+    )
+
     # markets API
     print("fetching all markets")
     print((await api.get_markets()).to_json())
