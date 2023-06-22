980a194808bb7cbcf65ff5e829e5210fa2ac5cdd
Kevin Chen <dumitru.cantemir@bloxroute.com>
2023-06-22 17:55:28 -0500

    TRAD-724 Added support for BloXroute Memo marking of Versioned transactions

--- bxsolana/examples/transaction_request_utils.py
+++ bxsolana/examples/transaction_request_utils.py
@@ -1,11 +1,9 @@
-import base64
 import random
 import sys
 from typing import Tuple
 
 from bxsolana_trader_proto import api as proto
 from bxsolana_trader_proto.common import OrderType
-from solders import hash as hs
 
 from .. import provider
 from .. import transaction
@@ -137,43 +135,33 @@ async def do_transaction_requests(
         )
     )
 
-    print("creating transactions with memo")
-    await create_transaction_with_memo(api)
+    print("marking transactions with memo")
+    await mark_transaction_with_memo(api)
 
 
-async def create_transaction_with_memo(api: provider.Provider):
+async def mark_transaction_with_memo(api: provider.Provider):
     private_key = transaction.load_private_key_from_env()
-
-    instruction = transaction.create_trader_api_memo_instruction("hi from dev")
-
-    recent_block_hash_resp = await api.get_recent_block_hash(
-        get_recent_block_hash_request=proto.GetRecentBlockHashRequest()
+    public_key = private_key.pubkey()
+    response = await api.post_trade_swap(
+        trade_swap_request=proto.TradeSwapRequest(
+            project=proto.Project.P_JUPITER,
+            owner_address=str(public_key),
+            in_token="USDT",
+            in_amount=0.01,
+            out_token="USDC",
+            slippage=0.01,
+        )
     )
-    recent_block_hash = hs.Hash.from_string(recent_block_hash_resp.block_hash)
-    instructions = [instruction]
 
-    tx_serialized = transaction.build_fully_signed_txn(
-        recent_block_hash, private_key.pubkey(), instructions, private_key
-    )
-    single_memo_txn = base64.b64encode(tx_serialized).decode("utf-8")
-    print("serialized memo single_memo_txn", single_memo_txn)
+    tx = response.transactions[0].content
+    tx = transaction.add_memo_to_serialized_txn(tx)
+
+    signed_tx = transaction.sign_tx_with_private_key(tx, private_key)
 
     post_submit_response = await api.post_submit(
         post_submit_request=proto.PostSubmitRequest(
-            transaction=proto.TransactionMessage(single_memo_txn),
+            transaction=proto.TransactionMessage(signed_tx),
             skip_pre_flight=True,
         )
     )
     print("signature for single memo txn", post_submit_response.signature)
-
-    double_memo_txn_signed = transaction.add_memo_to_serialized_txn(
-        single_memo_txn, "hi from dev2", private_key.pubkey(), private_key
-    )
-    print("double_memo_txn_signed", double_memo_txn_signed)
-    post_submit_response = await api.post_submit(
-        post_submit_request=proto.PostSubmitRequest(
-            transaction=proto.TransactionMessage(double_memo_txn_signed),
-            skip_pre_flight=True,
-        )
-    )
-    print("signature for double memo tx", post_submit_response.signature)
--- test/unit/transaction/test_memo.py
+++ test/unit/transaction/test_memo.py
@@ -1,35 +1,19 @@
-import base64
 import unittest
 
-import base58
-from solders.hash import Hash
-from solders.keypair import Keypair
 
 from bxsolana.transaction import memo
 
 # key generated for this test
 RANDOM_PRIVATE_KEY = "3KWC65p6AvMjvpR2r1qLTC4HVSH4jEFr5TMQxagMLo1o3j4yVYzKsfbB3jKtu3yGEHjx2Cc3L5t8wSo91vpjT63t"
-EXPECTED_TX = "AUtsDXEoPs/Le7/K1Q/nmExo+CpTpFqQgPMbGUSCBNAmZrD7hqbyg60kCgWQSq51dHg/s6ZHPJv4uh6Bx739hwwBAAECJtRexXabOwki+cTaDA2v4p93KEdHmPKORFtuewaoCqvzoQQP2CW/hYwd0lyG8/Zlm8M9Cro+eJpipSyuyZn3gQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADAgEAC2hpIGZyb20gZGV2AQAMaGkgZnJvbSBkZXYy"
+EXPECTED_TX = "AAEAAQMmRmIlQZ625bapZEp3haQ7Bu0r1zqVP0wTF9LYtsuMQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA86EED9glv4WMHdJchvP2ZZvDPQq6PniaYqUsrsmZ94GF+Ae/EKhvjYII9uq1QkZIuRVBHwVbIHdB+Y3tmQI1zgIBAAACAB9Qb3dlcmVkIGJ5IGJsb1hyb3V0ZSBUcmFkZXIgQXBp"
+EMPTY_TRANSACTION = "AAEAAQImRmIlQZ625bapZEp3haQ7Bu0r1zqVP0wTF9LYtsuMQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAhfgHvxCob42CCPbqtUJGSLkVQR8FWyB3QfmN7ZkCNc4BAQAA"
 
 
 class TestMemo(unittest.TestCase):
     def test_adding_memo_to_serialized_tx(self):
-        pkey_bytes = bytes(RANDOM_PRIVATE_KEY, encoding="utf-8")
-        pkey_bytes_base58 = base58.b58decode(pkey_bytes)
-        kp = Keypair.from_bytes(pkey_bytes_base58)
 
-        instruction = memo.create_trader_api_memo_instruction("hi from dev")
-
-        recent_block_hash = Hash.from_string("11111111111111111111111111111114")
-        instructions = [instruction]
-
-        tx_serialized = memo.build_fully_signed_txn(
-            recent_block_hash, kp.pubkey(), instructions, kp
-        )
-
-        txbase64 = base64.encodebytes(tx_serialized).decode("ascii")
         tx_bytes = memo.add_memo_to_serialized_txn(
-            txbase64, "hi from dev2", kp.pubkey(), kp
+            EMPTY_TRANSACTION
         )
 
         self.assertEqual(EXPECTED_TX, tx_bytes)
