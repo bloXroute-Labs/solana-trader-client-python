e4d09b280525675a304c02ee312ed561229dd180
etienneblxr <etienne.lunetta@bloxroute.com>
2024-03-01 02:12:30 -0800

    fix lint

--- bxsolana/provider/jsonrpc_patch.py
+++ bxsolana/provider/jsonrpc_patch.py
@@ -1,6 +1,7 @@
 import enum
 from jsonrpc.types.server_error import RpcErrorCode, message_map, RpcError
 
+
 class NewRpcErrorCode(enum.Enum):
     PARSE_ERROR = -32700
     INVALID_REQUEST = -32600
@@ -14,9 +15,10 @@ class NewRpcErrorCode(enum.Enum):
     def message(self) -> str:
         return message_map[self]
 
-RpcErrorCode = NewRpcErrorCode;
 
-message_map = {
+RpcErrorCode = NewRpcErrorCode  # noqa: F811
+
+message_map = {  # noqa: F811
     NewRpcErrorCode.PARSE_ERROR: "Parse error",
     NewRpcErrorCode.INVALID_REQUEST: "Invalid request",
     NewRpcErrorCode.METHOD_NOT_FOUND: "Invalid method",
@@ -30,6 +32,7 @@ message_map = {
 # Save the original from_json method
 original_from_json = RpcError.from_json
 
+
 # Define a new from_json method
 @classmethod
 def new_from_json(cls, payload: dict):
@@ -37,12 +40,13 @@ def new_from_json(cls, payload: dict):
     message = payload.get("message", "")
     data = payload.get("data")
     request_id = payload.get("id")
-    
+
     if code in [e.value for e in RpcErrorCode]:
         return cls(RpcErrorCode(code), request_id, data, message=message)
     else:
         # Fallback (shouldn't happen)
         return original_from_json(cls, payload)
+
 
 # Patch the RpcError class with the new from_json method
 RpcError.from_json = new_from_json
