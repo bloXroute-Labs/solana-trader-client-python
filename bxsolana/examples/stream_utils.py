bda33f1358a38f7b80b33bc067003f779a8549bb
Mahmoud Taabodi <mahmoud.taabodi@bloxroute.com>
2024-09-09 15:55:42 -0400

    fixing

--- bxsolana/examples/stream_utils.py
+++ bxsolana/examples/stream_utils.py
@@ -4,6 +4,25 @@ from .. import provider
 
 async def do_stream(api: provider.Provider, run_slow: bool = False):
     item_count = 0
+    print("streaming pump fun new tokens...")
+    async for response in api.get_pump_fun_new_tokens_stream(
+        get_pump_fun_new_tokens_stream_request=proto.GetPumpFunNewTokensStreamRequest()
+    ):
+        print(response.to_json())
+        async for sresponse in api.get_pump_fun_swaps_stream(
+                get_pump_fun_swaps_stream_request=proto.GetPumpFunSwapsStreamRequest(tokens=[response.mint])
+        ):
+            print(sresponse.to_json())
+            item_count += 1
+            if item_count == 1:
+                item_count = 0
+                break
+
+        item_count += 1
+        if item_count == 1:
+            item_count = 0
+            break
+
     print("streaming market depth updates...")
     async for response in api.get_market_depths_stream(
         get_market_depths_request=proto.GetMarketDepthsRequest(
