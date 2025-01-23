c0ea8cc894c990894d654e622f197b76b5f294a4
Lanikean <jose.bravo@bloxroute.com>
2025-01-23 12:46:12 -0700

    init

--- sdk.py
+++ sdk.py
@@ -18,7 +18,7 @@ from helpers import Endpoint, get_markets, get_pools, get_tickers, get_raydium_c
     get_new_raydium_pools_stream_cpmm, get_recent_blockhash_stream, get_pool_reserve_stream, \
     get_block_stream, get_priority_fee, get_priority_fee_stream, get_bundle_tip_stream, get_priority_fee_by_program_stream, get_token_accounts, \
     call_trade_swap, call_route_trade_swap, call_raydium_trade_swap, call_raydium_cpmm_trade_swap, \
-    call_raydium_clmm_trade_swap, call_jupiter_trade_swap, call_pump_fun_trade_swap, create_personal_tx_and_submit
+    call_raydium_clmm_trade_swap, call_jupiter_trade_swap, call_pump_fun_trade_swap, create_personal_tx_and_submit, call_submit_snipe 
 
 nest_asyncio.apply()
 init(autoreset=True)
@@ -96,6 +96,7 @@ ExampleEndpoints = {
     "jupiter_swap": Endpoint(func=call_jupiter_trade_swap, requires_additional_env_vars=True),
     "pump_fun_swap": Endpoint(func=call_pump_fun_trade_swap, requires_additional_env_vars=True),
     "create_custom_bloxroute_transfer": Endpoint(func=create_personal_tx_and_submit, requires_additional_env_vars=True),
+    "submit_snipe": Endpoint(func=call_submit_snipe, requires_additional_env_vars=True)
 }
 
 
